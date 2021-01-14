571b49014ca79a54cda2ec7ca2c23b5d3448dfaa
CedricPicron <cedric.picron@kuleuven.be>
2021-01-14 10:24:59 +0100

    Small rewrite in distributed.py

--- utils/distributed.py
+++ utils/distributed.py
@@ -27,10 +27,10 @@ def init_distributed_mode(args):
     args.gpu = int(os.environ['LOCAL_RANK'])
     torch.cuda.set_device(args.gpu)
 
-    print(f"| distributed init (rank {args.rank}): {args.dist_url}", flush=True)
-    torch.distributed.init_process_group(backend='nccl', init_method=args.dist_url,
-                                         world_size=args.world_size, rank=args.rank)
+    kwargs = {'backend': 'nccl', 'init_method': args.dist_url, 'world_size': args.world_size, 'rank': args.rank}
+    torch.distributed.init_process_group(**kwargs)
 
+    print(f"| distributed init (rank {args.rank}): {args.dist_url}", flush=True)
     set_master_only_printing(args.rank == 0)
 
 
