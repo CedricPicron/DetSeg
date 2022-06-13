20beb489b0886cf3776411a85d218481fafec028
CedricPicron <cedric.picron@kuleuven.be>
2022-06-13 14:06:59 +0200

    Implements the ApplyAll module

--- models/modules/utils.py
+++ models/modules/utils.py
@@ -8,9 +8,55 @@ from models.build import build_model, MODELS
 
 
 @MODELS.register_module()
+class ApplyAll(nn.Module):
+    """
+    Class implementing the ApplyAll module.
+
+    The ApplyAll module applies its underlying module to all inputs from the given input list.
+
+    Attributes:
+        module (nn.Module): Underlying module applied to all inputs from the input list.
+    """
+
+    def __init__(self, module_cfg):
+        """
+        Initializes the ApplyAll module.
+
+        Args:
+            module_cfg (Dict): Configuration dictionary specifying the underlying module.
+        """
+
+        # Initialization of default nn.Module
+        super().__init__()
+
+        # Build underlying module
+        self.module = build_model(module_cfg)
+
+    def forward(self, in_list, **kwargs):
+        """
+        Forward method of the ApplyAll module.
+
+        Args:
+            in_list (List): List with inputs to be processed by the underlying module.
+            kwargs (Dict): Dictionary of keyword arguments passed to the underlying module.
+
+        Returns:
+            out_list (List): List with resulting outputs of the underlying module.
+        """
+
+        # Apply underlying module to all inputs from input list
+        out_list = [self.module(input, **kwargs) for input in in_list]
+
+        return out_list
+
+
+@MODELS.register_module()
 class GetApplyInsert(nn.Module):
     """
     Class implementing the GetApplyInsert module.
+
+    The GetApplyInsert module applies its underlying module to the selected input from the given input list, and
+    inserts the resulting output back into the given input list.
 
     Attributes:
         get_id (int): Index selecting the input for the underlying module from a given input list.
