00a7de49db06732349ab5adea779eb63acd2ea03
CedricPicron <cedric.picron@kuleuven.be>
2023-05-05 13:09:09 +0200

    Adds possibility to use soft labels in BaseClsHead

--- models/heads/cls.py
+++ models/heads/cls.py
@@ -7,6 +7,7 @@ from torch import nn
 import torch.nn.functional as F
 
 from models.build import build_model, MODELS
+from structures.boxes import box_iou
 
 
 @MODELS.register_module()
@@ -17,11 +18,12 @@ class BaseClsHead(nn.Module):
     Attributes:
         logits (nn.Module): Module computing the classification logits.
         matcher (nn.Module): Optional module determining the target classification labels.
+        soft_label_type (str): Optional string containing the soft label type.
         loss (nn.Module): Module computing the classification loss.
         apply_ids (List): List with integers determining when the head should be applied.
     """
 
-    def __init__(self, logits_cfg, loss_cfg, matcher_cfg=None, apply_ids=None, **kwargs):
+    def __init__(self, logits_cfg, loss_cfg, matcher_cfg=None, soft_label_type=None, apply_ids=None, **kwargs):
         """
         Initializes the BaseClsHead module.
 
@@ -29,6 +31,7 @@ class BaseClsHead(nn.Module):
             logits_cfg (Dict): Configuration dictionary specifying the logits module.
             loss_cfg (Dict): Configuration dictionary specifying the loss module.
             matcher_cfg (Dict): Configuration dictionary specifying the matcher module (default=None).
+            soft_label_type (str): String containing the soft label type (default=None).
             apply_ids (List): List with integers determining when the head should be applied (default=None).
             kwargs (Dict): Dictionary of unused keyword arguments.
         """
@@ -45,7 +48,8 @@ class BaseClsHead(nn.Module):
         # Build loss module
         self.loss = build_model(loss_cfg)
 
-        # Set apply_ids attribute
+        # Set remaining attributes
+        self.soft_label_type = soft_label_type
         self.apply_ids = apply_ids
 
     def forward_pred(self, qry_feats, storage_dict, **kwargs):
@@ -73,14 +77,16 @@ class BaseClsHead(nn.Module):
         Forward loss method of the BaseClsHead module.
 
         Args:
-            storage_dict (Dict): Storage dictionary containing at least following keys (after matching):
+            storage_dict (Dict): Storage dictionary (possibly) containing following keys (after matching):
                 - cls_logits (FloatTensor): classification logits of shape [num_feats, num_labels];
                 - match_labels (LongTensor): match labels corresponding to each query of shape [num_queries];
                 - matched_qry_ids (LongTensor): indices of matched queries of shape [num_pos_queries];
-                - matched_tgt_ids (LongTensor): indices of corresponding matched targets of shape [num_pos_queries].
+                - matched_tgt_ids (LongTensor): indices of corresponding matched targets of shape [num_pos_queries];
+                - pred_boxes (Boxes): predicted 2D bounding boxes of size [num_feats].
 
-            tgt_dict (Dict): Target dictionary containing at least following key:
-                - labels (LongTensor): target class indices of shape [num_targets].
+            tgt_dict (Dict): Target dictionary (possibly) containing following keys:
+                - labels (LongTensor): target class indices of shape [num_targets];
+                - boxes (Boxes): target 2D bounding boxes of size [num_targets].
 
             loss_dict (Dict): Dictionary containing different weighted loss terms.
             analysis_dict (Dict): Dictionary containing different analyses (default=None).
@@ -93,6 +99,9 @@ class BaseClsHead(nn.Module):
 
             analysis_dict (Dict): Analysis dictionary containing following additional key (if not None):
                 - cls_acc (FloatTensor): classification accuracy of shape [].
+
+        Raises:
+            ValueError: Error when an invalid soft label type is provided.
         """
 
         # Perform matching if matcher is available
@@ -106,21 +115,43 @@ class BaseClsHead(nn.Module):
         matched_tgt_ids = storage_dict['matched_tgt_ids']
 
         # Get classification logits with corresponding targets
+        device = cls_logits.device
+        num_labels = cls_logits.size(dim=1)
+
         pos_cls_logits = cls_logits[matched_qry_ids]
         pos_cls_targets = tgt_dict['labels'][matched_tgt_ids]
 
+        if self.soft_label_type is None:
+            pos_cls_targets_oh = F.one_hot(pos_cls_targets, num_classes=num_labels)
+
+        elif self.soft_label_type == 'box_iou':
+            num_pos_matches = len(matched_qry_ids)
+            pos_cls_targets_oh = torch.zeros(num_pos_matches, num_labels, device=device)
+
+            if num_pos_matches > 0:
+                pred_boxes = storage_dict['pred_boxes'][matched_qry_ids].detach()
+                tgt_boxes = tgt_dict['boxes'][matched_tgt_ids]
+                ious = box_iou(pred_boxes, tgt_boxes).diag()
+
+                match_ids = torch.arange(num_pos_matches, device=device)
+                pos_cls_targets_oh[match_ids, pos_cls_targets] = ious
+
+        else:
+            error_msg = f"Invalid soft label type (got '{self.soft_label_type}') in BaseClsHead module."
+            raise ValueError(error_msg)
+
         neg_mask = match_labels == 0
         num_negs = neg_mask.sum().item()
-        num_labels = cls_logits.size(dim=1)
 
         neg_cls_logits = cls_logits[neg_mask]
         neg_cls_targets = torch.full([num_negs], num_labels-1, device=cls_logits.device)
+        neg_cls_targets_oh = F.one_hot(neg_cls_targets, num_classes=num_labels)
 
         cls_logits = torch.cat([pos_cls_logits, neg_cls_logits], dim=0)
         cls_targets = torch.cat([pos_cls_targets, neg_cls_targets], dim=0)
+        cls_targets_oh = torch.cat([pos_cls_targets_oh, neg_cls_targets_oh], dim=0)
 
         # Get classification loss
-        cls_targets_oh = F.one_hot(cls_targets, num_classes=num_labels)
         cls_loss = self.loss(cls_logits, cls_targets_oh)
 
         key_name = f'cls_loss_{id}' if id is not None else 'cls_loss'
