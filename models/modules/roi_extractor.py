4536e9f26f8606f585a42cb13f9fd9978ebe59e9
CedricPicron <cedric.picron@kuleuven.be>
2023-12-19 22:13:02 +0100

    Implements the MMDetRoIExtractor module (2)

--- models/modules/roi_extractor.py
+++ models/modules/roi_extractor.py
@@ -15,14 +15,16 @@ class MMDetRoIExtractor(nn.Module):
 
     Attributes:
         mmdet_roi_ext (nn.Module): Module containing the MMDetection RoI extractor.
+        out_key (str): String with key to store output RoI features in storage dictionary.
     """
 
-    def __init__(self, mmdet_roi_ext_cfg):
+    def __init__(self, mmdet_roi_ext_cfg, out_key='roi_feats'):
         """
         Initializes the MMDetRoIExtractor module.
 
         Args:
             mmdet_roi_ext_cfg (Dict): Configuration dictionary specifying the underlying MMDetection RoI extractor.
+            out_key (str): String with key to store output RoI features in storage dictionary (default='roi_feats').
         """
 
         # Initialization of default nn.Module
@@ -30,6 +32,9 @@ class MMDetRoIExtractor(nn.Module):
 
         # Build underlying MMDetection RoI extractor
         self.mmdet_roi_ext = build_model(mmdet_roi_ext_cfg)
+
+        # Set additional attribute
+        self.out_key = out_key
 
     def forward(self, storage_dict, **kwargs):
         """
@@ -45,7 +50,8 @@ class MMDetRoIExtractor(nn.Module):
             kwargs (Dict): Dictionary of unused keyword arguments.
 
         Returns:
-            roi_feats (FloatTensor): Extracted RoI features of shape [num_rois, feat_size, rH, rW].
+            storage_dict (Dict): Storage dictionary containing following additional key:
+                - {self.out_key} (FloatTensor): Extracted RoI features of shape [num_rois, feat_size, rH, rW].
         """
 
         # Retrieve desired items from storage dictionary
@@ -61,4 +67,7 @@ class MMDetRoIExtractor(nn.Module):
         roi_feat_maps = feat_maps[:self.mmdet_roi_ext.num_inputs]
         roi_feats = self.mmdet_roi_ext(roi_feat_maps, roi_boxes)
 
-        return roi_feats
+        # Store RoI features in storage dictionary
+        storage_dict[self.out_key] = roi_feats
+
+        return storage_dict
